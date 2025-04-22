3a594539fb67ff396c2bbc802357d5d70388f54f
deepali <deepali.sirsath1998@gmail.com>
2025-04-22 23:46:58 +0530

    fix: Improve TextField autofill styling
    This commit addresses an issue where the TextField autofill background color was not properly handled, especially in dark mode.
    
    - Modified the `.MuiInputBase-input:-webkit-autofill` style to use `transparent` for the background color and added a transition to ensure a smooth visual experience. This prevents the default yellow autofill background from appearing and interfering with the component's styling.
    - Removed commented-out code related to previous attempts to style the autofill, as it is no longer needed with the new approach.

--- src/components/common/textInputField/textInputField.style.ts
+++ src/components/common/textInputField/textInputField.style.ts
@@ -12,10 +12,7 @@ export const CharacterCountTypography = styled(Typography, {
       right: 10,
     };
   }
-
-  return {};
 });
-
 
 export const StyledTextField = styled(TextField)(({ theme }) => {
   return {
@@ -24,15 +21,6 @@ export const StyledTextField = styled(TextField)(({ theme }) => {
       zIndex: 0,
     },
 
-    // ".MuiInputBase-input:-webkit-autofill": {
-    //   WebkitBoxShadow:
-    //     theme.palette.mode === "dark"
-    //       ? `0 0 0 30px #111827 inset`
-    //       : `0 0 0 30px #fff inset`,
-    //   WebkitTextFillColor: theme.palette.text.primary,
-    //   caretColor: theme.palette.text.primary,
-    //   borderRadius: "inherit",
-    // },
     ".MuiInputBase-input:-webkit-autofill": {
       WebkitBoxShadow: "0 0 0 30px transparent inset !important",
       transition: "background-color 5000s ease-in-out 0s",
